ed14e8db34414b9bfae7bb18962b0c3d43b7ff4f
pemrouz <p3pedram@hotmail.com>
2015-06-14 20:00:51 +0100

    100% coverage

--- test.js
+++ test.js
@@ -1,28 +1,33 @@
 var expect = require('chai').expect
+  , client = require('client')
+  , shim = !client && polyfill()
   , raw = require('./')
   , is = require('is')
+  , node 
 
 describe('raw', function() {
 
-  it('should call querySelector with selector', function(){
-    var selector = '.class'
-      , result 
-      , fn = function(selector){ result = selector; }
-      , doc = { querySelector: fn }
+  before(function(){
+    /* istanbul ignore else */
+    if (!client) { node = document.body.firstElementChild }
+    else {
+      node = document.body.appendChild(document.createElement('div'))
+      node.className = 'class'
+      node.appendChild(document.createElement('li')).className = 'sub 1'
+    }
+  })
 
-    raw(selector, doc)
-    expect(result).to.equal(selector)
+  it('should call querySelector with selector', function(){
+    expect(raw('.sub', node).className).to.be.eql('sub 1')
   })
 
   it('should call querySelector with selector and global document', function(){
-    var selector = '.class'
-      , result 
-      , fn = function(selector){ result = selector; }
-      , doc = { querySelector: fn }
-
-    global.document = doc
-    raw(selector)
-    expect(result).to.equal(selector)
+    expect(raw('.class .sub').className).to.be.eql('sub 1')
   })
 
 })
+
+function polyfill(){
+  window = require("jsdom").jsdom('<div class="class"><li class="sub 1"></li></div>').defaultView
+  global.document = window.document
+}
